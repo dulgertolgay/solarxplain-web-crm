13e525fd40291f062a08c872d693ec991d64d60d
Tolgay Dülger <49791793+dulgertolgay@users.noreply.github.com>
2024-06-21 15:13:21 +0300

    feat: date-picker component

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import * as React from "react";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export interface DatePickerProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  type?: string;
+  placeholder?: string;
+}
+
+export const DatePicker = React.forwardRef<HTMLInputElement, DatePickerProps>(
+  ({ className, type, placeholder, ...props }, ref) => {
+    const [date, setDate] = React.useState<Date>();
+
+    return (
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? (
+              format(date, "PPP")
+            ) : (
+              <span>{placeholder || "Pick a date"}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    );
+  }
+);
